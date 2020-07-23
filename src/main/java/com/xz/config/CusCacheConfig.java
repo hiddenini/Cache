52f51fe07d5141e0d2c2334a3d571e71a6d33626
zjw <1300151235@qq.com>
2020-07-23 15:01:41 +0800

    测试不过期问题

--- src/main/java/com/xz/config/CusCacheConfig.java
+++ src/main/java/com/xz/config/CusCacheConfig.java
@@ -1,9 +1,7 @@
 package com.xz.config;
 
-import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.codec.JsonJacksonCodec;
-import org.redisson.config.Config;
 import org.redisson.spring.cache.CacheConfig;
 import org.redisson.spring.cache.RedissonSpringCacheManager;
 import org.springframework.cache.CacheManager;
@@ -11,7 +9,6 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -33,9 +30,9 @@ public class CusCacheConfig {
     CacheManager cacheManager(RedissonClient redissonClient) {
         Map<String, CacheConfig> config = new HashMap<String, CacheConfig>();
         // 创建一个名称为"testMap"的缓存，过期时间ttl为24分钟，同时最长空闲时maxIdleTime为12分钟。
-        config.put("testMap", new CacheConfig(24 * 60 * 1000, 12 * 60 * 1000));
-        config.put("CLIENT_CACHE", new CacheConfig(10 * 1000, 0));
-        config.put("ACCESS_TOKEN_CACHE", new CacheConfig(60 * 1000, 0));
+        //config.put("testMap", new CacheConfig(24 * 60 * 1000, 12 * 60 * 1000));
+        config.put("ZJW_CLIENT_CACHE", new CacheConfig(60 * 1000, 0));
+        config.put("ZJW_ACCESS_TOKEN_CACHE", new CacheConfig(60 * 1000, 0));
 
         return new RedissonSpringCacheManager(redissonClient, config, JsonJacksonCodec.INSTANCE);
     }
--- src/test/java/com/xz/CacheTest.java
+++ src/test/java/com/xz/CacheTest.java
@@ -11,13 +11,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cache.CacheManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
 @Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 public class CacheTest {
 
-    private static final String CLIENT_CACHE = "CLIENT_CACHE";
-    private static final String ACCESS_TOKEN_CACHE = "ACCESS_TOKEN_CACHE";
+    private static final String CLIENT_CACHE = "ZJW_CLIENT_CACHE";
+    private static final String ACCESS_TOKEN_CACHE = "ZJW_ACCESS_TOKEN_CACHE";
 
     @Autowired
     private CacheManager cacheManager;
@@ -26,12 +31,21 @@ public class CacheTest {
     public void put() {
         //cacheManager.getCache(CACHE_ACCESS_TOKEN_CLIENT).put(token.getAccessToken(), client);
 
-        for (int i = 0; i < 1; i++) {
+/*        for (int i = 0; i < 0; i++) {
+            Random random = new Random();
+            int j = random.nextInt(1000);
             Client client = new Client();
             client.setAppKey("aaa" + i);
             client.setAppSecret("bbb" + i);
             cacheManager.getCache(CLIENT_CACHE).put("client" + i, client);
-        }
+        }*/
+
+        Random random = new Random();
+        int j = random.nextInt(1000);
+        Client client = new Client();
+        client.setAppKey("aaa" + j);
+        client.setAppSecret("bbb" + j);
+        cacheManager.getCache(CLIENT_CACHE).put("client" + j, client);
     }
 
     @Test
@@ -49,13 +63,32 @@ public class CacheTest {
 
     @Test
     public void putToken() {
-        cacheManager.getCache(ACCESS_TOKEN_CACHE).put("token", AccessToken.builder().appKey("zzz").appSecret("yyy").build());
+        for (int i = 0; i < 10; i++) {
+            AccessToken accessToken = new AccessToken();
+            accessToken.setAppKey("zzz" + i);
+            accessToken.setAppSecret("yyy" + i);
+            cacheManager.getCache(ACCESS_TOKEN_CACHE).put("token" + i, accessToken);
+        }
     }
 
     @Test
     public void getToken() {
-        AccessToken token = cacheManager.getCache(ACCESS_TOKEN_CACHE).get("token", AccessToken::new);
+        AccessToken token = cacheManager.getCache(ACCESS_TOKEN_CACHE).get("token0", AccessToken::new);
         log.info("token:{}", JSON.toJSONString(token));
     }
+
+    @Test
+    public void ll() {
+        /**
+         * 时间戳转换成字符串
+         */
+        Long time = 1595403377669l;
+        SimpleDateFormat sf = null;
+        Date d = new Date(time);
+        sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println("d:" + sf.format(d));
+
+    }
+
 
 }
